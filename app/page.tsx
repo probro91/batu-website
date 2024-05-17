1c686501129a59b9adab40f9889ad59ac31c0768
probro91 <bozdener@gmail.com>
2024-05-17 00:46:30 -0400

    Set up second half of page need to fill with content

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,36 @@
 import ScrollIndicator from "./ui/scroll-down-icon";
+import MediaLinks from "./ui/media-links";
+import Image from 'next/image';
+import Link from "next/link"
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col justify-between">
       <div className="flex items-center justify-center h-screen">
         <div className="text-center">
-          <h1 className="text-6xl font-bold tracking-tight">Ali Batu Ozdener</h1>
+          <h1 id="nameHeader" className="text-6xl font-bold tracking-tight">Ali Batu Ozdener</h1>
           <p className="animate-fade-in">CS @ UGA</p>
         </div>
         <ScrollIndicator/>
+        
       </div>
+      <div className="flex flex-row justify h-screen">
+        <div className="flex flex-col text-left p-10 w-2/3 justify-between">
+          <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non felis convallis, tempus mauris eget, consequat ipsum. Nam nec elementum purus. Nullam ac turpis eleifend, mattis nibh in, viverra ante. Pellentesque nec nibh elit. Donec in purus massa. Sed lobortis metus non lacus rutrum, quis sagittis orci accumsan. Praesent sagittis nec tellus ut venenatis. Quisque non tortor a est cursus dapibus. Integer euismod arcu vitae tortor lacinia cursus. Vestibulum tincidunt mauris nisi, eu ultricies elit semper eu. Nulla facilisi. Maecenas commodo efficitur mauris, eu bibendum enim porta id. Etiam dictum dolor hendrerit tempus placerat. Nunc viverra augue vitae dolor fermentum.
+          </p>
+          <MediaLinks/>
+        </div>
+        <div className="relative w-1/3 p-8 hidden md:block">
+          <Image
+            src="/IMG_5119.jpg"
+            layout="fill"
+            objectFit="contain"
+            className="rounded-md pr-10"
+            alt="Picture of me"
+          />
+        </div>
+      </div>  
     </main>
-
-
   );
 }
+
--- app/ui/scroll-down-icon.tsx
+++ app/ui/scroll-down-icon.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
+'use client';
 
-const ScrollIndicator = () => {
-  return (
-    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 mb-8 animate-fade-in">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-8 w-8 animate-bounce"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M19 14l-7 7m0 0l-7-7m7 7V3"
-        />
-      </svg>
-    </div>
-  );
+import Link from 'next/link';
+import { useState, useEffect, useRef } from 'react';
+import clsx from 'clsx';
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+
+export default function ScrollIndicator() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const indicatorRef = useRef(null);
+
+    useEffect(() => {
+        const showNav = gsap.fromTo(
+            indicatorRef.current, {
+            opacity: 0,
+        }, {
+            opacity: 1,
+            duration: 0.4
+        }).progress(1);
+        ScrollTrigger.create({
+            start: "top top",
+            end: "max",
+            onUpdate: (self) => {
+                self.direction === -1 ?
+                    showNav.play()
+                    : showNav.reverse()
+            }
+        });
+    }, [])
+
+
+    return (
+            <div className={"absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-8 animate-fade-in"}
+            >
+                
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-8 w-8 animate-bounce"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="blue"
+                        //onClick={handleScroll}
+                        ref={indicatorRef}
+                    >
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M19 14l-7 7m0 0l-7-7m7 7V3"
+                        />
+                    </svg>
+            
+            </div>
+    );
 };
 
-export default ScrollIndicator;
