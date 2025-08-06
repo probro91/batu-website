70a08ff01fec424c3d7e29e8541ea8e87e1ed468
probro91 <bozdener@gmail.com>
2025-08-05 22:01:05 -0400

    Updating for new internship

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <div className="flex items-center justify-center h-screen">
         <div className="text-center">
           <h1 id="nameHeader" className="text-6xl font-bold tracking-tight">Ali Batu Ozdener</h1>
-          <p className="animate-fade-in">CS @ UGA</p>
+          <p className="animate-fade-in">MS AI @ UGA</p>
         </div>
         <ScrollIndicator/>
         
@@ -17,13 +17,44 @@ export default function Page() {
       <div className="h-screen relative flex flex-col">
         <div className="flex-grow flex flex-row justify">
           <div className="flex flex-col text-left pr-10 pl-10 pt-10 w-2/3 justify">
-          <p className="text-lg">{"Hi, thanks for visiting my website! I am Ali Batu Ozdener, currently an undergraduate studying computer science at the University of Georgia, with plans to pursue a master's degree in Artificial Intelligence. I love being engaged on campus, so I have a variety of roles and responsibilities across different organizations. Firstly, I am a research assistant under Dr. Deepak Mishra, where I research the application of NeRFs in arid and polar climates. Next, I serve as the sponsorship director for "}
-            <a href="https://ugahacks.com" target="_blank" className="text-blue-500 hover:underline">UGAHacks</a>
-            {", the flagship hackathon of the University of Georgia. I also volunteer for Girls Who Code, teaching K-12 students about computer science. Serving in these different roles has given me great insight into the field of computer science, helping guide me in my professional career. I have experience as a Fullstack Engineer from my two times interning at Macy's where I worked on applications for Macy's contact center. Outside of school, I love playing guitar, hiking, and spending time with my family. Please check out my resume and feel free to reach out to me through LinkedIn or email!"}
+          <p className="text-lg">
+            {"Hi, thanks for visiting my website! I am Ali Batu Ozdener. In May 2025, I graduated Summa Cum Laude with a Bachelor’s in Computer Science from the University of Georgia, and I am now pursuing a Master’s degree in Artificial Intelligence through the university’s accelerated program, graduating May 2026. I love being engaged on campus, so I have a variety of roles and responsibilities across different organizations. I am a research assistant under Dr. Deepak Mishra, where I research into the field of "}
+            <a
+              href="https://www.matthewtancik.com/nerf"
+              target="_blank"
+              className="text-blue-500 hover:underline"
+            >
+              NeRFs
+            </a>
+            {". I also serve as the lead director of "}
+            <a
+              href="https://ugahacks.com"
+              target="_blank"
+              className="text-blue-500 hover:underline"
+            >
+              UGAHacks
+            </a>
+            {", the flagship hackathon of the University of Georgia. In addition, I have volunteered for Girls Who Code in the past, teaching K–12 students about computer science. Serving in these different roles has given me great insight into the field of computer science, helping guide me in my professional career. I have experience as a Fullstack Engineer from my internship at "}
+            <a
+              href="https://aws.amazon.com"
+              target="_blank"
+              className="text-blue-500 hover:underline"
+            >
+              Amazon Web Services
+            </a>
+            {" last summer working in the Billing organization, as well as my two internships at "}
+            <a
+              href="https://www.macys.com"
+              target="_blank"
+              className="text-blue-500 hover:underline"
+            >
+              Macy's
+            </a>
+            {" where I worked on applications for Macy’s contact center. Outside of school, I love playing guitar, hiking, and spending time with my family. Please check out my resume and feel free to reach out to me through LinkedIn or email!"}
           </p>
-            <div className="grid h-1/2 justify items-center">
-              <MediaLinks/>
-            </div>
+          <div className="grid h-1/2 justify items-center">
+            <MediaLinks/>
+          </div>
             
           </div>
           <div className="relative w-1/3 pr-8 pl-8 pt-8d hidden md:block">
--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -4,7 +4,7 @@ export default function Component() {
         <footer className="pb-4" >
             <div className="container max-w-7xl mx-auto border-t border-gray-200 dark:border-gray-700 pt-4">
                 <p className="text-sm text-gray-500 dark:text-gray-400 text-center">
-                    © 2024 Ali Batu Ozdener. All rights reserved.
+                    © 2025 Ali Batu Ozdener. All rights reserved.
                 </p>
             </div>
         </footer>
